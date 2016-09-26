4c267af69003b0fa71dccf5543607d2137f6482e
Andrija Barbarosa <andrija.barbarosa@gmail.com>
2016-09-26 12:35:53 +0200

    test openshift config connection

--- dbtest.php
+++ dbtest.php
@@ -5,7 +5,7 @@ $dbuser = getenv("MYSQL_USER");
 $dbpwd = getenv("MYSQL_PASSWORD");
 $dbname = getenv("MYSQL_DATABASE");
 
-$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+$connection = new mysqli($dbhost, user8PM, gAWG5hq63VUAytdJ, sampledb);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
